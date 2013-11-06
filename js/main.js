14df38f4621755fa7800551a787ad76a6acb4c7e
unknown <anhnt38@gmail.com>
2013-11-06 15:25:44 +0700

    add images and jquery plugin

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
 (function($){
 	$(function(){
 		$('#menu').stop().superfish();
+
+
+		// Revolution slider
+
+		if ($.fn.cssOriginal != undefined)
+		$.fn.css = jQuery.fn.cssOriginal;
+		$('.fullwidthbanner').revolution({
+			delay: 4000,
+			startheight: 396,
+			startwidth: 1170,
+
+			hideThumbs: 200,
+
+			thumbWidth: 100,
+			thumbHeight: 50,
+			thumbAmount: 5,
+
+			navigationType: "both",
+			navigationArrows: "nexttobullets",
+			navigationStyle: "round",
+			touchenabled: "on",
+			onHoverStop: "on",
+
+			navOffsetHorizontal: 0,
+			navOffsetVertical: 20,
+
+			shadow: 1,
+			fullWidth: "on"
+		});
 	})
 })(jQuery);
